0b7d0d16eb02b2215b808ae6152397c1bb77fe51
QuijoteShin <quijote.shin@gmail.com>
2017-03-01 03:08:43 -0300

    [psandoval] ++ home with table filtering and paging from source

--- psandoval/assets/app/bch-test.js
+++ psandoval/assets/app/bch-test.js
@@ -17,16 +17,115 @@ angular.module('Collapsible', [])
 
   var self = this;
   self.isCollapsed = true;
+  self.collapLabel ="Abrir";
 
   self.collapseToggle =  function() {
     self.isCollapsed = !self.isCollapsed;
+    if(self.isCollapsed)
+      self.collapLabel = "Abrir";
+    else
+      self.collapLabel = "Cerrar";
   };
 
 });
 
+
+modules.push('panels');
+angular.module('panels', modules)
+.directive('panelCollapsible', function() {
+   return {
+       templateUrl: 'template/panel-collapsible.html'
+   }
+});
+
+
+modules.push('inputTable');
+angular.module('inputTable', [])
+.controller('inputTableCtrl', function ($scope, $http, $filter) {
+
+    var self = this;
+
+    self.fs = {};
+    self.fs.select1Opts = [{"val": 1, "desc": "Opción 1"}];
+    self.fs.general_search = '';
+
+    self.maxSize = 5;
+    self.filteredItems = [];
+    self.itemsPerPage = 5;
+    self.pagedItems = [];
+    self.currentPage = 0;
+    self.totalItems = 0;
+    self.totalItemsFiltered = 0.
+    self.items = [];
+
+    $http.get('resource/table.json')
+    .then(function(res){
+      self.items = res.data;
+      self.totalItems = self.items.length;
+      self.search();
+    });
+
+
+    /*
+    $scope.$watch('currentPage', function(newValue, oldValue, scope) {
+        console.log('search');
+    }, objectEquality);
+    */
+
+    self.pageChanged = function() {
+      self.pageData();
+    };
+
+    var strainer = function (haystack, needle) {
+        if (!needle) {
+            return true;
+        }
+
+        if(haystack === parseInt(haystack, 10))
+          return haystack.toString().indexOf(needle) !== -1;
+        else
+          return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
+    };
+
+    self.search = function () {
+
+        self.filteredItems = $filter('filter')(self.items, function (item) {
+            for(var col in item) {
+                if (strainer(item[col], self.fs.general_search)) {
+                    return true;
+                }
+            }
+            return false;
+        });
+
+        self.currentPage = 0;
+        self.totalItemsFiltered = self.filteredItems.length;
+        self.pageData();
+
+        console.log('search');
+    };
+
+    self.pageData = function () {
+
+        page = self.currentPage - 1;
+        if(page < 0)
+          page = 0;
+
+        rangeFrom = page * self.itemsPerPage;
+        rangeTo   = (page + 1 ) * self.itemsPerPage;
+
+        self.pagedItems = self.filteredItems.slice(
+          rangeFrom,
+          rangeTo
+        );
+
+        //console.log(rangeFrom, rangeTo, self.itemsPerPage, page, self.pagedItems);
+    };
+})
+
+;
+
 angular.module('bch.test', modules)
-
-
 .config(function($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/');
